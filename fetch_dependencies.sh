0975a9c39ff27f87c3d35fb480cdebadb61e66a1
Jonathan Piat <piat.jonathan@gmail.com>
2015-08-04 21:39:41 +0000

    New script to fetch the robot dependencies

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,12 +1,45 @@
 #!/bin/sh
 
-sudo apt-get install gcc g++ libx11-dev libxt-dev libxext-dev libgraphicsmagick1-dev libcv-dev libhig$
+#sudo apt-get update
+#sudo apt-get install gcc g++ libx11-dev libxt-dev libxext-dev libgraphicsmagick1-dev libcv-dev
 
-git clone https://github.com/jpiat/robidouille.git
-git clone https://github.com/jpiat/motor_hat_clib.git
+if [ ! -d dependencies ]; then
+	mkdir dependencies
+fi
 
-git clone https://github.com/raspberrypi/userland.git
+cd dependencies
+
+if [ ! -d userland ]; then
+	git clone https://github.com/raspberrypi/userland.git
+else
+	cd userland
+	git pull
+	cd ..
+fi
+
+if [ ! -d robidouille ]; then
+	git clone https://github.com/jpiat/robidouille.git
+else
+	cd robidouille
+	git pull
+	cd ..
+fi
+
+if [ ! -d motor_hat_clib ]; then
+	git clone https://github.com/jpiat/motor_hat_clib.git
+else
+	cd motor_hat_clib
+	git pull
+	cd ..
+fi
+
 cd userland
-./buildme
+USERLAND_PWD=${PWD}
+#./buildme
+cd ..
 
-cd 
+echo ${USERLAND_PWD}
+cd robidouille/raspicam_cv
+mkdir objs
+USERLAND_ROOT=${USERLAND_PWD} make
+sudo make install
