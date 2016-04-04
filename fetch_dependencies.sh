3b2ac24b22c184c966a71d099222b3f926e929aa
Jonathan Piat <piat.jonathan@gmail.com>
2016-04-04 15:36:36 +0200

    Moving to OpenCV3.0 built from source with NEON enabled

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -5,6 +5,13 @@ BASE_DIR=`pwd`
 sudo apt-get update
 sudo apt-get install gcc g++ libx11-dev libxt-dev libxext-dev libgraphicsmagick1-dev libcv-dev liblz4-dev
 
+#required by OpenCV
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libdc1394-22-dev libilmbase6 openexr
+#end of required by OpenCV
 
 if [ ! -d dependencies ]; then
 	mkdir dependencies
@@ -16,11 +23,6 @@ if([ ! -d opencv ]; then
 	git clone https://github.com/Itseez/opencv.git
 	cd opencv/
 	git checkout tags/3.0.0
-	mkdir release
-	cd release
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D ENABLE_NEON=ON..
-	make -j4
-	sudo make install
 fi
 
 cd ${BASE_DIR}/dependencies
@@ -52,6 +54,16 @@ else
 	git pull
 	cd ..
 fi
+
+cd ${BASE_DIR}/dependencies
+
+cd opencv/
+git checkout tags/3.0.0
+mkdir release
+cd release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D ENABLE_NEON=ON ..
+make -j4
+sudo make install
 
 cd ${BASE_DIR}/dependencies
 
