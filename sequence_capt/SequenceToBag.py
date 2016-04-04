6b01527943b5f235e09c2c5ebbf37e62c59492a4
Jonathan Piat <piat.jonathan@gmail.com>
2016-04-04 11:52:53 +0200

    New Python file to generate a valid ros bag from captured images

--- sequence_capt/SequenceToBag.py
+++ sequence_capt/SequenceToBag.py
@@ -4,11 +4,18 @@ import roslib
 import rospy
 roslib.load_manifest('sensor_msgs')
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
 
 import ImageFile
 
+model = 'plumb_bob'
+
+D = [0.09588004493901636, -0.2751577208590459, 0.0007265931787498115, -0.007979244347949572, 0.0]
+K = [627.4842111637764, 0.0, 301.42612443545056, 0.0, 626.5832675966146, 249.65402637073538, 0.0, 0.0, 1.0]
+R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+P = [629.0787353515625, 0.0, 296.5435491975986, 0.0, 0.0, 632.731201171875, 249.43851519734744, 0.0, 0.0, 0.0, 1.0, 0.0]
 
 G_2_MPSS=9.80665
 ACC_RANGE=4.0
@@ -16,17 +23,16 @@ GYRO_RANGE=500.0
 
 def acc_raw_to_ms(d):
 	g = (ACC_RANGE/32768.0)*float(d)
-	g = g*G_2_MPSS
+	g = g*G_2_MPSS #convert to m/s^-2
 	return g
 
 def gyro_raw_to_rads(d):
 	g = (GYRO_RANGE/32768.0)*float(d) # deg/s
-	g = g*(math.pi/180.0)
+	g = math.radians(g)#g*(math.pi/180.0)#convert to radian/s
 	return g
 
 def create_vector3(x, y, z):
 	v = Vector3()
-	# I guess the units will be like that, but I don't know
 	v.x = x
 	v.y = y
 	v.z = z	
@@ -67,12 +73,31 @@ def GetImageFromFile(im_path):
 	Img.header.stamp = Stamp
 	Img.width = im.size[0]
 	Img.height = im.size[1]
-	Img.encoding = "mono8"
+	Img.encoding = "mono8" #needs to be changed to rgb8 for rgb images
+	Img.step=Img.width #some nodes may complains ...
 	Img.header.frame_id = "camera"
-	Img_data = [pix for pix in im.getdata()]
+	Img_data = list(im.getdata()) #works for mono channels images (grayscale)
+	#Img_data = [pix for pix in im.getdata()]
 	#  Img_data = [pix for pixdata in im.getdata() for pix in pixdata]
 	Img.data = Img_data
 	return (im_stamp, Stamp, Img)
+
+def GetCameraInfo(width, height):
+	cam_info = CameraInfo()
+	cam_info.width = width
+	cam_info.height = height
+	cam_info.distortion_model = model
+	#cam_info.D = [0.0]*5
+	#cam_info.K = [0.0]*9
+	#cam_info.R = [0.0]*9
+	#cam_info.P = [0.0]*12
+	cam_info.D = D
+        cam_info.K = K
+        cam_info.R = R
+        cam_info.P = P
+	cam_info.binning_x = 0
+	cam_info.binning_y = 0
+	return cam_info
 
 def GetFilesFromDir(dir):
     '''Generates a list of files from the directory'''
@@ -84,6 +109,7 @@ def GetFilesFromDir(dir):
             for f in files:
                 if os.path.splitext(f)[1] in ['.pgm']:
                     all.append( os.path.join( path, f ) )
+    print 'found '+str(len(all))+' images'
     return all
 
 def CreateMonoBag(imgs,imu,bagname):
@@ -94,17 +120,21 @@ def CreateMonoBag(imgs,imu,bagname):
     while line[0] == '%': #skipping comment lines
 	line = fimu.readline()
     try:
+	print 'Complete 0% \r',
         for i in range(len(imgs)):
-            print("Adding %s" % imgs[i])
+            #print("Adding %s" % imgs[i])
             (im_stamp, Stamp, Img) = GetImageFromFile(imgs[i])
-            while True :
-                (ts, ImuStamp, imu) = GetImuFromLine(line)
+            while line :
+		(ts, ImuStamp, imu) = GetImuFromLine(line)
 		bag.write('imu', imu, ImuStamp)
 		if ts > im_stamp:
 			break
 		line = fimu.readline()   
-
+            print 'Complete '+str((float(i)/float(len(imgs)))*100.0)+'% \r' ,
+            cam_info = GetCameraInfo(Img.width, Img.height)
+            bag.write('camera/camera_info', cam_info, Stamp)
             bag.write('camera/image_raw', Img, Stamp)
+        print 'Complete 100% \n\r'
     finally:
         bag.close()       
 
